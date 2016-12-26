c98c69a293ca75c244a17795788b9829f9bc7728
NakajimaKouhei <nakajima-kohei@ixui.co.jp>
2016-12-26 10:26:02 +0900

    #24
    フォームクラスの作成
    入力されていない時のエラーメッセージ実装

--- src/main/java/jp/co/ixui/controller/book/BookAdminForm.java
+++ src/main/java/jp/co/ixui/controller/book/BookAdminForm.java
@@ -2,6 +2,8 @@ package jp.co.ixui.controller.book;
 
 import javax.validation.constraints.NotNull;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,25 +12,31 @@ import lombok.Setter;
 public class BookAdminForm {
 		//ISBN
 		@NotNull
+		@NotEmpty(message = "ISBNを入力してください。")
 		private String isbn;
 
 		//書籍名
 		@NotNull
+		@NotEmpty(message = "書籍名を入力してください。")
 		private String bookName;
 
 		//著者
 		@NotNull
+		@NotEmpty(message = "著者を入力してください。")
 		private String author;
 
 		//出版日
 		@NotNull
+		@NotEmpty(message = "出版日を入力してください。")
 		private String publishDate;
 
 		//出版社
 		@NotNull
+		@NotEmpty(message = "出版社を入力してください。")
 		private String publisher;
 
 		//内容
 		@NotNull
+		@NotEmpty(message = "書籍の内容を入力してください。")
 		private String content;
 }
