8db8115e65e7a9802236c8a1a5affb6d91f3fb08
NakajimaKouhei <nakajima-kohei@ixui.co.jp>
2016-12-28 10:17:15 +0900

    #37
    書籍取得テスト

--- src/main/resources/db/migration/V1_2__create-m_book.sql
+++ src/main/resources/db/migration/V1_2__create-m_book.sql
@@ -10,3 +10,8 @@ create table m_book(
     regist_time date,
     update_time date
 ) CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+insert into m_book
+values ("978-4844336389", "スッキリわかるJava入門 第2版", "中山 清喬", "2014/8/7",
+	"インプレス", "本書は、Javaの基礎から初学者には難しいとされるオブジェクト指向まで、膨らむ疑問にしっかり対応し、Javaプログラミングの「なぜ?」がわかる解説と約300点の豊富なイラストで、楽しく・詳しく・スッキリとマスターできる構成となっています。",
+	"9999", NULL, CURRENT_DATE(), NULL);
