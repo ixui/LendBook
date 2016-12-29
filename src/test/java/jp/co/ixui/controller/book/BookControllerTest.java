009d2a64e8cb42c20c8b7b521d814edfc36deb3b
NakajimaKouhei <nakajima-kohei@ixui.co.jp>
2016-12-29 14:28:15 +0900

    #45
    書籍画面ユニットテスト
    コントローラ/サービスクラス

--- src/test/java/jp/co/ixui/controller/book/BookControllerTest.java
+++ src/test/java/jp/co/ixui/controller/book/BookControllerTest.java
@@ -1,5 +1,6 @@
 package jp.co.ixui.controller.book;
 
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -14,6 +15,9 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
+
+import jp.co.ixui.domain.MstBook;
+import jp.co.ixui.service.BookDisplayService;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -34,18 +38,17 @@ public class BookControllerTest {
 	//正常
 	@Test
 	public void 書籍登録フォーム送信テスト() throws Exception{
+    ResultActions resultActions = mockMvc.perform(post("/admin/book")
+    		.contentType(MediaType.APPLICATION_FORM_URLENCODED)
+    		.param("bookName", "TEST")
+    		.param("author", "author")
+    		.param("publisher", "publish")
+    		.param("isbn", "123-1234567890")
+    		.param("publishDate", "2015/8/25")
+    		.param("content", "Test"));
 
-        ResultActions resultActions = mockMvc.perform(post("/admin/book")
-        		.contentType(MediaType.APPLICATION_FORM_URLENCODED)
-        		.param("bookName", "TEST")
-        		.param("author", "author")
-        		.param("publisher", "publish")
-        		.param("isbn", "123-1234567890")
-        		.param("publishDate", "2015/8/25")
-        		.param("content", "Test"));
-
-        resultActions.andExpect(model().hasNoErrors());
-	}
+    resultActions.andExpect(model().hasNoErrors());
+}
 
 	//異常
 	@Test
@@ -62,5 +65,18 @@ public class BookControllerTest {
 
         //エラー
         resultActions.andExpect(model().hasErrors());
+	}
+
+	@Test
+	public void 書籍ページアクセステスト() throws Exception{
+
+		MstBook mstBook = new MstBook();
+		mstBook.setIsbn("123-1234567890");
+
+		BookDisplayService service = mock(BookDisplayService.class);
+		when(service.selectBook(mstBook.getIsbn())).thenReturn(mstBook);
+
+		mockMvc.perform(get("/book/123-1234567890"))
+			.andExpect(status().isOk());
 	}
 }
