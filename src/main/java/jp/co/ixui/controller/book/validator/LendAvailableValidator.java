d93b5d78225f2c7b9a354c973fc8277199dab63a
NakajimaKouhei <nakajima-kohei@ixui.co.jp>
2017-01-10 12:32:39 +0900

    #55
    バリデーションの処理をサービスクラスで作成

--- src/main/java/jp/co/ixui/controller/book/validator/LendAvailableValidator.java
+++ src/main/java/jp/co/ixui/controller/book/validator/LendAvailableValidator.java
@@ -22,25 +22,6 @@ public class LendAvailableValidator implements ConstraintValidator<LendAvailable
 	public boolean isValid(Lend value, ConstraintValidatorContext context) {
 
 		int bookStockId = value.getBookStockId();
-
-		//貸出履歴に該当する蔵書が存在しない(Null)ならtrue
-		try{
-			Lend lend = service.selectLendHistory(bookStockId);
-			String lendHistory = lend.getLendDate();
-		}catch(NullPointerException e){
-			return true;
-		}
-
-		//貸出履歴に返却されていない蔵書が存在しないなら(Null)ならtrue
-		try{
-			Lend lend = service.selectReturnDateBook(bookStockId);
-			String lendReturn = lend.getReturnDate();
-		}catch(NullPointerException e){
-			return true;
-		}
-
-		//それ以外は貸出不可
-		return false;
+		return service.isLendable(bookStockId);
 	}
-
 }
