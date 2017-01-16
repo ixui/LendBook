81572fc61bbf649dac45092c4ad5a0b150d3fbf9
NakajimaKouhei <nakajima-kohei@ixui.co.jp>
2017-01-16 15:17:50 +0900

    #60
    ReturnDueDateOverValidatorのJavaDoc作成

--- src/main/java/jp/co/ixui/controller/book/validator/ReturnDueDateOverValidator.java
+++ src/main/java/jp/co/ixui/controller/book/validator/ReturnDueDateOverValidator.java
@@ -11,18 +11,29 @@ import jp.co.ixui.controller.book.validator.annotation.ReturnDueDateOver;
 import jp.co.ixui.domain.Lend;
 import jp.co.ixui.service.BookService;
 
+/**
+ * 貸出時の返却予定日バリデーション
+ * @author NAKAJIMA
+ *
+ */
 public class ReturnDueDateOverValidator implements ConstraintValidator<ReturnDueDateOver, Lend> {
 
 	@Autowired
 	BookService service;
 
+	/**
+	 * 初期設定なし
+	 */
 	@Override
 	public void initialize(ReturnDueDateOver constraintAnnotation) {
 	}
 
+	/**
+	 * Lendに格納されている返却予定日を使い、返却予定日の妥当性を判定する。<br>
+	 * 返却予定日が正常ならtrue,問題があればfalseを返す。
+	 */
 	@Override
 	public boolean isValid(Lend value, ConstraintValidatorContext context) {
 		return service.isReturnDueDateOver(value.getReturnDueDate());
 	}
-
 }
