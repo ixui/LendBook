508feafb9add50c956158ee2c2c23e8bd603c60d
NakajimaKouhei <nakajima-kohei@ixui.co.jp>
2017-01-16 16:02:59 +0900

    #60
    MainController DataBaseConfigのJavaDoc作成

--- src/main/java/jp/co/ixui/DataBaseConfig.java
+++ src/main/java/jp/co/ixui/DataBaseConfig.java
@@ -7,9 +7,25 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/**
+ * DB接続用クラス<br>
+ * 環境変数を利用してDBへとアクセス
+ * @author NAKAJIMA
+ *
+ */
 @Configuration
 public class DataBaseConfig {
 
+	/**
+	 * DBのURL,ID,PASSWORDを環境変数を利用してアクセスする。<br>
+	 * 変数に、環境変数に入力されているURL,ID,PASSWORDを入れるので、<br>
+	 * 事前に環境変数を設定しておく必要があります。<br>
+	 * 取得した環境変数をDataSoruceBuilderを用いて、
+	 * DBにアクセスする設定をする。
+	 * 「@Configuration」を利用しているのでアクセス時にこの設定ファイルが読み込まれる。
+	 *
+	 * @return DBにアクセスする設定を作成
+	 */
 	//プロパティの設定を各フィールドにインジェクション
 	@Bean
 	@ConfigurationProperties("spring.datarsource")
--- src/main/java/jp/co/ixui/controller/MainController.java
+++ src/main/java/jp/co/ixui/controller/MainController.java
@@ -22,6 +22,12 @@ public class MainController {
 	@Autowired
 	BookService bookService;
 
+	/**
+	 * index画面を表示する
+	 * /indexと/は同じ画面を表示
+	 * @param mav 画面以外に渡す情報なし。
+	 * @return index.htmlを渡す。
+	 */
 	//トップページ
 	@RequestMapping(value = {"/", "/index"}, method=RequestMethod.GET)
 	public ModelAndView top(
@@ -30,6 +36,12 @@ public class MainController {
 		return mav;
 	}
 
+	/**
+	 * ログイン画面を表示する。
+	 * POSTはSpringSecurityを実装したクラスにて行う。
+	 * @param mav 画面以外に渡す情報なし。
+	 * @return login.htmlを渡す。
+	 */
 	//ログインページ
 	@RequestMapping(value = "/login", method=RequestMethod.GET)
 	public ModelAndView login(
@@ -38,6 +50,15 @@ public class MainController {
 		return mav;
 	}
 
+	/**
+	 * メイン画面を表示する。
+	 * SpringSecurity側のチェックを通過した場合にこの画面に自動的に遷移する。
+	 *
+	 * 新着書籍/おすすめ書籍を表示させる。
+	 * 表示させる書籍数は変数limitを使い設定する。
+	 * @param mav 書籍情報を格納
+	 * @return main.htmlと格納した情報を渡す。
+	 */
 	//ログイン後メイン
 	@RequestMapping(value = "/main", method=RequestMethod.GET)
 	public ModelAndView main(
